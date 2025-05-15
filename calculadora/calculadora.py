0aa1127b57f3c30a4c2245c307f6431a65e488d3
Brenda <bremeirah@gmail.com>
2025-05-15 18:02:45 -0300

    Testes criados, prontos para aplicar na calculadora que será construída

---
+++ calculadora/calculadora.py
@@ -0,0 +1,5 @@
+def somar_numeros (num1, num2):
+    return num1 + num2
+
+def subtrair_numeros (num1, num2):
+    return num1 - num2
---
+++ calculadora/tests/test_calculadora.py
@@ -0,0 +1,26 @@
+from calculadora.calculadora import somar_numeros, subtrair_numeros
+
+def test_somar_numeros_soma_com_input ():
+    actual_result = somar_numeros (num1 = 8, num2 = 2)
+    assert actual_result == 10
+
+def test_subtrair_numeros_com_input ():
+    actual_result = subtrair_numeros (num1 = 9, num2 = 9)
+    assert actual_result == 0    
+    
+def test_dividir_numeros_com_input ():
+    actual_result = dividir_numeros (num1 = 9, num2 = 9)
+    assert actual_result == 1 
+
+def test_multiplicar_numeros_com_input ():
+    actual_result = multiplicar_numeros (num1 = 9, num2 = 9)
+    assert actual_result == 81
+    
+def test_elevar_numeros_com_input ():
+    actual_result = elevar_numeros (num1 = 2, num2 = 3)
+    assert actual_result == 27
+    
+def test_extrair_raiz_quadrada_numeros_com_input ():
+    actual_result = extrair_raiz_quadrada (num1 = 81)
+    assert actual_result == 9 
+    
