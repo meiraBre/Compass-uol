30e20f0b71e4b644202065d3b3a256bd76bd4f02
Brenda <bremeirah@gmail.com>
2025-05-15 18:24:15 -0300

    Resolvendo conflitos de organização das pastas para importar as funções

--- calculadora/tests/test_calculadora.py
+++ calculadora/tests/test_calculadora.py
@@ -1,4 +1,4 @@
-from calculadora.calculadora import somar_numeros, subtrair_numeros
+from calculadora.calculadora import somar_numeros, subtrair_numeros, dividir_numeros, multiplicar_numeros, elevar_numeros, extrair_raiz_quadrada
 
 def test_somar_numeros_soma_com_input ():
     actual_result = somar_numeros (num1 = 8, num2 = 2)
@@ -18,7 +18,7 @@ def test_multiplicar_numeros_com_input ():
     
 def test_elevar_numeros_com_input ():
     actual_result = elevar_numeros (num1 = 2, num2 = 3)
-    assert actual_result == 27
+    assert actual_result == 8
     
 def test_extrair_raiz_quadrada_numeros_com_input ():
     actual_result = extrair_raiz_quadrada (num1 = 81)
